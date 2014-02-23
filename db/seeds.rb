11f5bdcda5add6cea3bfd4e5ba1bb0b0b310c34e
bmackin <mackin.beth@gmail.com>
2014-02-23 17:49:12 -0500

    Add kaminari to gemfile and modify seeds file to create 100 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,87 +1,3 @@
-Product.create!(
-	:name 						=> "The Terror"
-	:description 			=> "by Dan Simmons: Dan Simmons writes with the salty grace and precision of Patrick OBrian. But in piling supernatural nightmare upon historical nightmare, layering mystery upon mystery, he has produced a turbocharged vision of popular doom. -Men's Journal Greeted with excited critical praise, this extraordinary novel-inspired by the true story of two ice ships that disappeared in the Arctic Circle during an 1845 expedition-swells with the heart-stopping suspense and heroic adventure that have won Dan Simmons praise as a writer who not only makes big promises but keeps them (Seattle Post-Intelligencer). THE TERROR chills readers to the core."
-	:price_in_cents 	=> "995"
-	:url 							=> "http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-73.jpg"
-	)
-
-Product.create!(
-	:name 						=> "The Boat"
-	:description 			=> "by Nam Le: In the magnificent opening story, a young writer is urged by his friends to mine his father’s experiences in Vietnam–and what seems at first a satire of turning one’s life into literary commerce becomes a transcendent exploration of the ties between father and son. “Cartagena” provides a visceral glimpse of life in Colombia as a fourteen-year-old hit man faces the ultimate test. In “Meeting Elise” an aging New York painter mourns his body’s decline as he prepares to meet his daughter on the eve of her Carnegie Hall debut. The title story returns us to Vietnam, to a fishing trawler crowded with refugees, where a young woman’s bond with a mother and her small son forces both women to a harrowing decision."
-	:price_in_cents 	=> "1095"
-	:url 							=> "http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-051.png"
-	)
-
-Product.create!(
-	:name 						=> "The Letters of Noel Coward"
-	:description 			=> "by Noel Coward: Writers labor to come up with lines half as good as those Noël Coward dropped into the mailbox every day—I felt that some sort of scene was necessary to celebrate my first entrance into America, so I said, 'Little lamb, who made thee,' to a customs official. The playwright, actor and songwriter is in fine form in these missives, telegrams and poems (he would rhyme almost anything, even communications to his business manager), presented along with return mail from friends and luminaries. Day (Coward on Film: The Cinema of Noel Coward) arranges the well-chosen selections in roughly chronological order with some unobtrusive narrative context; at times he spotlights a lifelong correspondence with a single person to flesh out Coward's relationships, such as with Gertrude Lawrence. Coward's voice is charming, whimsical, sharp-eyed and canny, often alternating, in the showbiz way, between effusive warmth (letter to Tallulah Bankhead: Thank you very much, darling, for all your sweetness and your insane generosity) and cutting putdown (letter about Tallulah Bankhead: a conceited slut). A true intellectual of the stage, his comments on the nitty-gritty of writing, pacing, character and acting technique are incisive. Fans of Coward's plays and students of 20th-century theater will be fascinated, but casual readers will also find an entertaining browse"
-	:price_in_cents 	=> "1545"
-	:url 							=> "http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-07.png
-"
-	)
-
-Product.create!(
-	:name 						=> "The Bedside Book of Beasts"
-	:description 			=> "by Graeme Gibson: The intricate, complex connection between the hunter and the hunted has defined animal life on Earth throughout time. In The Bedside Book of Beasts, Graeme Gibson gathers from all eras and cultures works of art and literature that capture the power, grace, and inventiveness of both predators and their natural prey. Here are myths, fables, poetry, generous excerpts from nature and travel writing, journals, sacred texts, and works of fiction. There are vivid descriptions of noteworthy predators — including the big cats, bears, wolves — but also the small but voracious praying mantis. Gibson also brings to life the experiences, strategies, and emotions of vulnerable prey, and paints intriguing portraits of such legendary evil beasts as the Minotaur, Grendel, and the Biblical Leviathan. All of this is enhanced by a breathtaking array of art, both traditional and contemporary, as well as scientific, religious, and mythological drawings, paintings, and woodcuts."
-	:price_in_cents 	=> "1295"
-	:url 							=> "http://www.youthedesigner.com/wp-content/uploads/2010/03/beautiful-book-covers-08.jpg"
-	)
-
-# Product.create!(
-# 	:name 						=> ""
-# 	:description 			=> ""
-# 	:price_in_cents 	=> ""
-# 	:url 							=> ""
-# 	)
-
-# Product.create!(
-# 	:name 						=> ""
-# 	:description 			=> ""
-# 	:price_in_cents 	=> ""
-# 	:url 							=> ""
-# 	)
-
-# Product.create!(
-# 	:name 						=> ""
-# 	:description 			=> ""
-# 	:price_in_cents 	=> ""
-# 	:url 							=> ""
-# 	)
-
-# Product.create!(
-# 	:name 						=> ""
-# 	:description 			=> ""
-# 	:price_in_cents 	=> ""
-# 	:url 							=> ""
-# 	)
-
-# Product.create!(
-# 	:name 						=> ""
-# 	:description 			=> ""
-# 	:price_in_cents 	=> ""
-# 	:url 							=> ""
-# 	)
-
-# Product.create!(
-# 	:name 						=> ""
-# 	:description 			=> ""
-# 	:price_in_cents 	=> ""
-# 	:url 							=> ""
-# 	)
-
-# Product.create!(
-# 	:name 						=> ""
-# 	:description 			=> ""
-# 	:price_in_cents 	=> ""
-# 	:url 							=> ""
-# 	)
-
-# Product.create!(
-# 	:name 						=> ""
-# 	:description 			=> ""
-# 	:price_in_cents 	=> ""
-# 	:url 							=> ""
-# 	)
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -89,8 +5,25 @@ Product.create!(
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# Picture.create!(
-# 	:title	=> "The old church on the coast of White sea",
-# 	:artist	=> "Sergey Ershov",
-# 	:url		=> "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-# 	)
+
+
+User.destroy_all
+Product.destroy_all
+
+User.create!({
+	email: "example@bitmakerlabs.com",
+	name: "Example User",
+	password: "pass",
+	password_confirmation: "pass"
+	}
+)
+
+100.times do |i|
+
+	Product.create({
+		name: "Produt#{i}",
+		description: "Description#{i}",
+		price_in_cents: i
+	}
+)
+end
